d45358fc453ba0e0e42cb7032c81dfbf562545bc
Binozo <entwickler@binozoworks.de>
2024-01-07 12:21:52 +0100

    fine-tuned speaker configuration

--- pkg/speaker/speaker.go
+++ pkg/speaker/speaker.go
@@ -6,15 +6,15 @@ import (
 )
 
 const CardNr = 0
-const DeviceNr = 25
+const DeviceNr = 23
 
 // GetDevice returns the pre-configured speaker alsa device
 func GetDevice() tinyalsa.AlsaDevice {
 	return tinyalsa.NewDevice(CardNr, DeviceNr, pcm.Config{
-		Channels:    2,
-		SampleRate:  16000,
-		PeriodSize:  512,
-		PeriodCount: 4,
-		Format:      tinyalsa.PCM_FORMAT_S24_LE,
+		Channels:    1,
+		SampleRate:  48000,
+		PeriodSize:  1024,
+		PeriodCount: 2,
+		Format:      tinyalsa.PCM_FORMAT_S16_LE,
 	})
 }
